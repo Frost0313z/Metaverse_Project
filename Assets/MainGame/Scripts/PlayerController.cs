2e18821735e95bd27a0eac12d49d473309c4fbc5
Frost0313z <msong7618@gmail.com>
2025-05-04 16:14:38 +0900

    feat: 기초 맵 구현
    플레이어가 이동할 맵을 설계하고 장식했습니다.

--- Assets/MainGame/Scripts/PlayerController.cs
+++ Assets/MainGame/Scripts/PlayerController.cs
@@ -8,18 +8,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody; // 충돌 구현해야 하니까
-    [SerializeField] private SpriteRenderer characterRenderer; // 캐릭터 스프라이트 이미지 필요하니까
+    private Rigidbody2D _rigidbody;
+    [SerializeField] private SpriteRenderer characterRenderer;
     private Vector2 knockback = Vector2.zero;
     [SerializeField] private float characterMoveSpeed = 1f;
-    [SerializeField] private float characterKnockback = 0.5f;
-    private float knockbackDuration = 0.0f;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
-    
+
     private void OnMove(InputValue inputValue)
     {
         Vector2 direction = inputValue.Get<Vector2>().normalized;
@@ -33,25 +31,19 @@ public class PlayerControler : MonoBehaviour
             characterRenderer.flipX = false;
         }
 
-        if(knockbackDuration > 0f) // 넉백
-        {
-            direction *= characterKnockback;
-            direction += knockback;
-        }
-
         _rigidbody.velocity = direction;
     }
 
 
     private void OnInteract(InputValue inputValue)
     {
-        // F키 누르면 상호작용 하도록 구현현
+        // F키 누르면 상호작용 하도록 구현
 
     }
 
     private void OnJump(InputValue inputValue)
     {
-        // 그라운드 만들고 리지드 바디 넣어야함함
+        // 그라운드 만들고 리지드 바디 넣어야함
         // 점프하는 애니메이션 출력하기
     }
 
